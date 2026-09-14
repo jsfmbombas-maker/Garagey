a8bc6b0256aa9424abd2a19d0b48705b10bb8bcd
jsfmbombas-maker <jsfm.bombas@gmail.com>
2026-09-14 22:13:09 +0200

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,20 @@
+const CACHE='garagey-v28-8-create-form';
+const CORE=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
