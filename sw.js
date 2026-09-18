3291b70f659f8fd9800d911e21c66b9c635b0a01
jsfmbombas-maker <jsfm.bombas@gmail.com>
2026-09-18 21:10:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='garagey-v35-32';
+const CACHE='garagey-v35-33';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
